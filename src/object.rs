df36f3a3cdc41ac5491f7c5cabbf69b3834d208a
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-02-29 22:31:09 +0545

    name added

--- src/object.rs
+++ src/object.rs
@@ -1,22 +1,34 @@
 use crate::position::Position;
 
 pub struct Object{
+    name: String,
     position: Position,
     children: Vec<Object>,
 }
 
 impl Object{
-    pub fn new_with_x_y(_x: i32, _y: i32) -> Object {
+    pub fn new_with_x_y(_name: &str, _x: i32, _y: i32) -> Object {
         Object {
+            name: _name.to_string(),
             position: Position::new_with_x_y(_x, _y),
             children: Vec::new(),
         }
     }
-    pub fn new() -> Object{
+    pub fn new(_name: &str) -> Object{
         Object{
+            name: _name.to_string(),
             position: Position::new(),
             children: Vec::new(),
         }
+    }
+    pub fn name(&self) -> String {
+        self.name.clone()
+    }
+    pub fn rename_to(&mut self, _name: &str){
+      while self.name.len() > 0{
+          self.name.remove(0);
+      }
+      self.name.push_str(_name)
     }
     pub fn x(&self) -> i32 {
         self.position.x()
