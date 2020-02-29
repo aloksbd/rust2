8d9a0ad5ef355e561e7fc3e67146464be809717d
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-02-29 22:31:09 +0545

    name added

--- src/main.rs
+++ src/main.rs
@@ -2,4 +2,14 @@ mod position;
 mod object;
 
 fn main() {
+    let mut o = object::Object::new("o");
+    println!("{} {} {}",o.x(),o.y(),o.name());
+
+    o = object::Object::new_with_x_y("oo",10, 10);
+    println!("{} {} {}",o.x(),o.y(),o.name());
+
+    o.set_y(2);
+    o.set_x(3);
+    o.rename_to("no");
+    println!("{} {} {}",o.x(),o.y(),o.name());
 }
