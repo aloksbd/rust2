28da8bef2fd4b7c2a22164a5de831255f3060d0d
aloksbd <30898579+aloksbd@users.noreply.github.com>
2020-03-02 17:50:50 +0545

    add and remove child

--- src/main.rs
+++ src/main.rs
@@ -12,4 +12,23 @@ fn main() {
     o.set_x(3);
     o.rename_to("no");
     println!("{} {} {}",o.x(),o.y(),o.name());
+
+    let c1 = object::Object::new("c1");
+    let c2 = object::Object::new("c2");
+    let c3 = object::Object::new("c1");
+    let c4 = object::Object::new("c4");
+    println!("{}",o.add_child(c1.clone()));
+    println!("{}",o.add_child(c2.clone()));
+    println!("{}",o.add_child(c3.clone()));
+    println!("{}",o.add_child(c4.clone()));
+    o.print_children();
+
+    println!("{}",o.remove_child(c1));
+    o.print_children();
+    println!("{}",o.remove_child(c2));
+    o.print_children();
+    println!("{}",o.remove_child(c4));
+    o.print_children();
+    println!("{}",o.remove_child(c3));
+    o.print_children();
 }
